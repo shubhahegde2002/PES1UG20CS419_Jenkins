8a2758377349fda6ee9e9dd66ba9880ba4d0b1a6
Shubha Hegde <73905298+shubhahegde2002@users.noreply.github.com>
2023-02-16 22:02:27 +0530

    Update hello.cpp

--- Jenkins_lab-main/main/hello.cpp
+++ Jenkins_lab-main/main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, PES1UG20CS419\n";
+  cout << "Hello, File Changed\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
